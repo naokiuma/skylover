7fe8c7c83fd7619f9d8560f0e471c5cb229d1a1f
nao <>
2019-11-28 00:28:34 +0900

    $image_url についてassert調整

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -11,9 +11,11 @@
   $image_url = str_replace('public/', 'storage/', $top_post->image_url);
   ?>
 
+
   <div class="jumbotron jumbotron-fluid">
     <div class="container billbord">
       <h3 class="display-6 top_title"><?php echo $top_post->title ?></h3>
+      <img src="{{ asset('<?php echo $image_url ?>') }}" alt="">
       <img src ="/{{ $image_url }}" class="img-fluid image-m top_main_img" alt="Responsive image"><br>
       <span class="ol_badge badge-pill badge-success top_main_category"><?php echo $top_post->category->category_name ?></span>
       <span class="time_info top_main_info"><?php echo $top_post->created_at ?>posted.</span>
