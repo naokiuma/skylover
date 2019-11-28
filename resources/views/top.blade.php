f6a92cd55434e8c0fbaaea66c982ab0c1a5637a2
nao <>
2019-11-28 11:54:30 +0900

    image fix

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -9,14 +9,17 @@
   $top_post = $posts[$num];
   //$image_url = $top_post->image_url;
   $image_url = str_replace('public/', 'storage/', $top_post->image_url);
+  //print_r($top_post);
+   print_r($image_url);
   ?>
 
 
   <div class="jumbotron jumbotron-fluid">
     <div class="container billbord">
       <h3 class="display-6 top_title"><?php echo $top_post->title ?></h3>
-      <img src="{{ asset('<?php echo $image_url ?>') }}" alt="">
-      <img src ="/{{ $image_url }}" class="img-fluid image-m top_main_img" alt="Responsive image"><br>
+
+      <img src="/{{ $image_url }}" alt="">
+      <img src ="{{ $image_url }}" class="img-fluid image-m top_main_img" alt="Responsive image"><br>
       <span class="ol_badge badge-pill badge-success top_main_category"><?php echo $top_post->category->category_name ?></span>
       <span class="time_info top_main_info"><?php echo $top_post->created_at ?>posted.</span>
 
