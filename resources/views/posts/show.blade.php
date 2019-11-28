377c91b1338d0e907decc9f3583a5e2ff3a00496
nao <>
2019-11-28 12:15:24 +0900

    image修正

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -12,7 +12,7 @@
   <div class="jumbotron-fluid">
     <div class="container billbord">
       <h3 class="display-6 top_title"><?php echo $post->title ?></h3>
-      <img src ="/{{ $image_url }}" class="img-fluid image-l top_main_img" alt="Responsive image"><br>
+      <img src ="../{{ $image_url }}" class="img-fluid image-l top_main_img" alt="Responsive image"><br>
       <span class="ol_badge badge-pill badge-success top_main_category"><?php echo $post->category->category_name ?></span>
       <span class="time_info top_main_info"><?php echo $post->created_at ?>posted.</span>
 
